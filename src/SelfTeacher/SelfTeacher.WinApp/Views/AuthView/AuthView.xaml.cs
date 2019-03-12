4acb7b0094517ced61a08dc58836df6714da7d08
Guschin Roman <romanshadow3@gmail.com>
2019-03-12 18:39:52 +0400

    Added Prism windows appication

---
+++ src/SelfTeacher/SelfTeacher.WinApp/Views/AuthView/AuthView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SelfTeacher.WinApp.Views.AuthView
+{
+    /// <summary>
+    /// Interaction logic for AuthView
+    /// </summary>
+    public partial class AuthView : UserControl
+    {
+        public AuthView()
+        {
+            InitializeComponent();
+        }
+    }
+}
