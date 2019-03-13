579a896c10bd80b127083f70c63551875fb41682
Guschin Roman <romanshadow3@gmail.com>
2019-03-13 16:19:13 +0400

    Added new view for auth  and domain and infrasctucture package for win app

--- src/SelfTeacher/SelfTeacher.WinApp/ViewModels/MainWindowViewModel.cs
+++ src/SelfTeacher/SelfTeacher.WinApp/ViewModels/MainWindowViewModel.cs
@@ -5,10 +5,17 @@ namespace SelfTeacher.WinApp.ViewModels
     public class MainWindowViewModel : BindableBase
     {
         private string _title = "Self teacher";
+        private string _name = "";
         public string Title
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
         }
 
         public MainWindowViewModel()
---
+++ src/SelfTeacher/SelfTeacher.WinApp/Views/AuthView/AuthForm.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SelfTeacher.WinApp.Views.AuthView
+{
+    /// <summary>
+    /// Interaction logic for AuthForm
+    /// </summary>
+    public partial class AuthForm : UserControl
+    {
+        public AuthForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
